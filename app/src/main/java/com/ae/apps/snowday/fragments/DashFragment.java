af610e7322f70fcddd16d950c27cd41d8f42ca32
midhunhk <midhunhk@gmail.com>
2016-01-21 01:07:41 +0530

    code cleanup

--- app/src/main/java/com/ae/apps/snowday/fragments/DashFragment.java
+++ app/src/main/java/com/ae/apps/snowday/fragments/DashFragment.java
@@ -28,6 +28,7 @@ import android.widget.Toast;
 import com.ae.apps.snowday.R;
 import com.ae.apps.snowday.utils.CameraCallback;
 import com.ae.apps.snowday.utils.CameraPreview;
+import com.ae.apps.snowday.utils.SnowChecker;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -44,7 +45,7 @@ import java.util.Random;
 public class DashFragment extends android.support.v4.app.Fragment {
 
     public static final String SNOW_DAY = "/SnowDay/";
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+
     private static final int SEE_ACTION_REQUEST_CODE = 5060;
     private Context mContext;
     private boolean isCheckingForSnow = false;
@@ -58,7 +59,6 @@ public class DashFragment extends android.support.v4.app.Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -82,28 +82,23 @@ public class DashFragment extends android.support.v4.app.Fragment {
         File snowDayPicturesDir = new File(directory);
         boolean mkdirsResult = snowDayPicturesDir.mkdirs();
 
-        // Request the OS for an image from the Camera, this is a standard approach but will be deprecated
+        final SnowChecker snowChecker = new SnowChecker(mContext);
+
         Button btnSeeMore = (Button) mLayout.findViewById(R.id.btnSeeMore);
+
         btnSeeMore.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                String fileName = getImageFileName(directory);
 
-                File targetFile = new File(fileName);
+                // Request the OS for an image from the Camera, this is a standard approach but we don't want this
 
-                try{
-                    targetFile.createNewFile();
-                } catch (IOException e){
-                    Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-
-                Uri outputFileUri = Uri.fromFile(targetFile);
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-
-                startActivityForResult(intent, SEE_ACTION_REQUEST_CODE);
+                // Intent intent = snowChecker.getDeviceCamera(directory);
+                // startActivityForResult(intent, SEE_ACTION_REQUEST_CODE);
+                Snackbar.make(mLayout, R.string.str_no_updates, Snackbar.LENGTH_SHORT)
+                        .show();
             }
         });
+
 
         // Take image using CameraAPI - this is what we want with this app
         Button btnSeeMoreMore = (Button) mLayout.findViewById(R.id.btnSeeMore2);
@@ -111,114 +106,24 @@ public class DashFragment extends android.support.v4.app.Fragment {
 
             @Override
             public void onClick(View view) {
-
-
-                String fileName = getImageFileName(directory);
-
-                // http://developer.android.com/training/camera/cameradirect.html
-                // http://stackoverflow.com/questions/10775942/android-sdk-get-raw-preview-camera-image-without-displaying-it/10776349#10776349
-
-                // http://stackoverflow.com/questions/2386025/taking-picture-from-camera-without-preview/14227517#14227517
-                final SurfaceView preview = new SurfaceView(mContext);
-                preview.getHolder().addCallback(new SurfaceHolder.Callback() {
-                    @Override
-                    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-
-                        if(false == isCheckingForSnow){
-                            isCheckingForSnow = true;
-                            Camera camera = null;
-                            try {
-                                camera = Camera.open();
-
-                                try {
-                                    // preview is a dummy surface view that is never attached to the screen
-                                    camera.setPreviewDisplay(preview.getHolder());
-                                } catch (IOException ex) {
-                                    isCheckingForSnow = false;
-                                    throw new RuntimeException(ex);
-                                }
-
-                                // Start preview to a dummy surface view before taking the picture
-                                camera.startPreview();
-
-                                camera.takePicture(null, null, new Camera.PictureCallback() {
-                                    @Override
-                                    public void onPictureTaken(byte[] bytes, Camera camera) {
-                                        camera.release();
-
-                                        // Decode and save the image
-                                        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                                        if(null != bitmap){
-                                            File file = new File(getImageFileName(directory));
-
-                                            try{
-                                                FileOutputStream fos = new FileOutputStream(file);
-                                                bitmap.compress(Bitmap.CompressFormat.JPEG, 80, fos);
-
-                                                fos.flush();
-                                                fos.close();
-
-                                                // Fix for orientation issues
-                                                // http://stackoverflow.com/questions/11674816/android-image-orientation-issue-with-custom-camera-activity?lq=1
-                                            } catch(Exception ex){
-
-                                            }
-                                        }
-
-                                        // Show a SnackBar as feedback on completing this task successfully
-                                        Snackbar.make(mLayout, R.string.snowday_snack, Snackbar.LENGTH_SHORT)
-                                                .show();
-                                    }
-                                });
-                            }catch(Exception ex) {
-                                if (null != camera) {
-                                    camera.release();
-                                    ;
-                                }
-                                isCheckingForSnow = false;
-                                throw new RuntimeException(ex);
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-                    }
-
-                    @Override
-                    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-
-                    }
-                });
-
-                // This requires the SYSTEM_ALERT_WINDOW permission
-                WindowManager windowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
-                WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(1, 1,
-                        WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
-                        0, PixelFormat.UNKNOWN);
-                windowManager.addView(preview, layoutParams);
+                snowChecker.checkForSnow(directory, mLayout);
             }
+
         });
 
+        // Assign a click event for the FAB
         FloatingActionButton floatingActionButton = (FloatingActionButton) mLayout.findViewById(R.id.myFAB);
         floatingActionButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
-
+                // snowChecker.checkForSnow(directory, mLayout);
+                Snackbar.make(mLayout, R.string.str_no_updates, Snackbar.LENGTH_SHORT)
+                        .show();
             }
         });
 
         return mLayout;
-    }
-
-    @NonNull
-    private String getImageFileName(String directory) {
-        Date currentTime = Calendar.getInstance().getTime();
-
-        // construct the file name
-        return directory + DATE_FORMAT.format(currentTime) + ".jpg";
     }
 
     @Override
